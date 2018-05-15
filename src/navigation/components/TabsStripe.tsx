de066a8d8764139136b929ea225711c24995f4e3
Deseteral <deseteral@gmail.com>
2018-05-15 23:09:44 +0200

    Code styling fix

--- src/navigation/components/TabsStripe.tsx
+++ src/navigation/components/TabsStripe.tsx
@@ -14,37 +14,39 @@ interface TabsStripeProps {
 
 function TabsStripe(props: TabsStripeProps) : JSX.Element {
   const { currentView, extended, onTabChange } = props;
-    const tabs = [
-      {
-        name: 'library',
-        view: View.LIBRARY,
-      }, {
-        name: 'maps',
-        view: View.MAPS,
-      }
-    ];
+  const tabs = [
+    {
+      name: 'library',
+      view: View.LIBRARY,
+    }, {
+      name: 'maps',
+      view: View.MAPS,
+    },
+  ];
 
-    return (
-      <TabsStripeContainer>
-        {tabs.map(tab => (
-          <TabItem
-            onClick={() => onTabChange(tab.view)}
-            active={currentView === tab.view}
-            shouldHide={!extended}
-          >
-            {tab.name}
-          </TabItem>
-        ))}
-      </TabsStripeContainer>
-    );
-  }
+  return (
+    <TabsStripeContainer>
+      {tabs.map(tab => (
+        <TabItem
+          onClick={() => onTabChange(tab.view)}
+          active={currentView === tab.view}
+          shouldHide={!extended}
+        >
+          {tab.name}
+        </TabItem>
+      ))}
+    </TabsStripeContainer>
+  );
 }
 
-export default connect((state: AppState, ownProps: TabsStripeProps) => ({
-  currentView: state.currentView,
-  extended: ownProps.extended,
-}), (dispatch) => ({
-  onTabChange: (nextView: View) => dispatch(viewChange(nextView)),
-}))(TabsStripe);
+export default connect(
+  (state: AppState, ownProps: TabsStripeProps) => ({
+    currentView: state.currentView,
+    extended: ownProps.extended,
+  }),
+  (dispatch) => ({
+    onTabChange: (nextView: View) => dispatch(viewChange(nextView)),
+  }),
+)(TabsStripe);
 
 export { TabsStripeProps };
