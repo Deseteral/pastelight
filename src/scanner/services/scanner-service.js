efea3c6ff59072222ab02cf9ae7bff550f709695
Deseteral <deseteral@gmail.com>
2018-07-16 23:27:01 +0200

    Scanner service skeleton

---
+++ src/scanner/services/scanner-service.js
@@ -0,0 +1,32 @@
+import recursiveReaddir from 'recursive-readdir';
+import { buildPhotoItem } from './photo-item-builder';
+
+async function getAcceptableFiles(directoryPath) {
+  const supportedPhotoFormats = [
+    'jpg', 'jpeg', // TODO: Move this somewhere else
+  ];
+
+  const ignoredFilesMatch = supportedPhotoFormats.map(extension => `!*.${extension}`);
+  return recursiveReaddir(directoryPath, ignoredFilesMatch);
+}
+
+async function addToDatabase(libraryItem) {
+  console.log(libraryItem);
+}
+
+async function processFile(filePath, fileScannedCallback) {
+  const item = await buildPhotoItem(filePath);
+  await addToDatabase(item);
+  fileScannedCallback();
+}
+
+async function scanDirectory(directoryPath, scannerPrelightCompletedCallback, fileScannedCallback) {
+  const files = await getAcceptableFiles(directoryPath);
+
+  const totalFileCount = files.length;
+  scannerPrelightCompletedCallback(totalFileCount);
+
+  Promise.all(files.map(filePath => processFile(filePath, fileScannedCallback)));
+}
+
+export default { scanDirectory };
