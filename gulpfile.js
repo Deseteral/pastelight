d1d907ff1d920c9cce270019bccdc5845070ad56
Deseteral <deseteral@gmail.com>
2019-06-20 23:32:17 +0200

    Set correct window name

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 const { parallel, src, dest } = require('gulp');
+const replace = require('gulp-replace');
+const packageJson = require('./package.json');
+
+const APPLICATION_NAME = `${packageJson.name} ${packageJson.version}`;
 
 function copyWindowHtml() {
-  return src('src/windows/*.html').pipe(dest('build/windows'));
+  return src('src/windows/*.html')
+    .pipe(replace('{APPLICATION_NAME}', APPLICATION_NAME))
+    .pipe(dest('build/windows'));
 }
 
 exports.default = parallel(copyWindowHtml);
