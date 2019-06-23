b9b15233d5a4326af40608ed05aa2c50e687d1af
Deseteral <deseteral@gmail.com>
2019-06-23 08:57:58 +0200

    Code fixes to use only index module imports

--- src/navigation/index.ts
+++ src/navigation/index.ts
@@ -1,7 +1,14 @@
 import ViewRouter from './components/ViewRouter';
 import NavigationBar from './components/NavigationBar';
 
+import currentView, { CurrentViewState } from './reducers/current-view';
+
 export {
   ViewRouter,
   NavigationBar,
+};
+
+export {
+  currentView,
+  CurrentViewState,
 };
