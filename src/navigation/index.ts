e2dfa3fd760ec60d45b6bd2a02184323bfad9126
Deseteral <deseteral@gmail.com>
2019-12-31 01:20:22 +0100

    Index module comments

--- src/navigation/index.ts
+++ src/navigation/index.ts
@@ -4,5 +4,8 @@ import NavigationBar from './components/NavigationBar';
 import NavigationProvider from './state/NavigationProvider';
 import useNavigation from './state/use-navigation';
 
+// UI components
 export { ViewRouter, NavigationBar };
+
+// State management
 export { NavigationProvider, useNavigation };
