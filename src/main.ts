4b64826498bc3fa2b12837ff4009220b9830193f
Deseteral <deseteral@gmail.com>
2020-01-08 23:48:25 +0100

    Removed unnecessary arrow function

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { app } from 'electron';
 import { createMainWindow } from './windows';
 
-app.on('ready', () => createMainWindow());
+app.on('ready', createMainWindow);
 app.on('window-all-closed', () => app.quit());
