a30b8720fe23c1f6ec3f7561f5bb874999c43ee2
Deseteral <deseteral@gmail.com>
2020-01-11 12:22:08 +0100

    Simplify building HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,9 @@
 const { parallel, src, dest } = require('gulp');
-const replace = require('gulp-replace');
 const packageJson = require('./package.json');
 
-const APPLICATION_NAME = `${packageJson.name} ${packageJson.version}`;
-
 function copyWindowHtml() {
-  return src('src/windows/**/*.html')
-    .pipe(replace('{APPLICATION_NAME}', APPLICATION_NAME))
-    .pipe(dest('build/windows'));
+  return src('src/**/*.html')
+    .pipe(dest('build'));
 }
 
 exports.default = parallel(copyWindowHtml);
