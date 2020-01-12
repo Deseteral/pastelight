382775c87233af29a927bf73f9fe719cf5efbc28
Deseteral <deseteral@gmail.com>
2020-01-12 13:22:04 +0100

    Raw styled button

--- src/elements/Button.tsx
+++ src/elements/Button.tsx
@@ -1,7 +1,13 @@
-import * as React from 'react';
 import styled from 'styled-components';
 
-const StyledButton = styled.button`
+interface ButtonProps {
+  onClick: () => void;
+}
+
+const Button = styled.button.attrs((props: ButtonProps) => ({
+  type: 'button',
+  onClick: props.onClick,
+}))`
   font-size: 16px;
   text-transform: uppercase;
   text-align: center;
@@ -26,16 +32,6 @@ const StyledButton = styled.button`
     box-shadow: 0 5px 15px rgba(0,0,0,.05), 0 4px 10px var(--color-primary-dark);
   }
 `;
-
-interface ButtonProps {
-  onClick: () => void;
-}
-
-const Button: React.FunctionComponent<ButtonProps> = ({ onClick, children }) => (
-  <StyledButton type="button" onClick={onClick}>
-    {children}
-  </StyledButton>
-);
 
 export default Button;
 export { ButtonProps };
