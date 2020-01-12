dfaa18d0f4b008f213b50c0ee6348a0688e070af
Deseteral <deseteral@gmail.com>
2020-01-12 19:54:14 +0100

    Welcome screen rename

--- src/windows/index.ts
+++ src/windows/index.ts
@@ -1,4 +1,4 @@
 import createAppWindow from './app-window/app-window';
-import createWelcomeScreenWindow from './welcome-screen/welcome-screen';
+import createWelcomeScreenWindow from './welcome-screen-window/welcome-screen-window';
 
 export { createAppWindow, createWelcomeScreenWindow };
