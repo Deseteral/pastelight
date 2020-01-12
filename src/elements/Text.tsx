9229596a69cbc1f05ba7d65c54e7d28340aa4545
Deseteral <deseteral@gmail.com>
2020-01-12 11:50:42 +0100

    Info message for empty recent list

--- src/elements/Text.tsx
+++ src/elements/Text.tsx
@@ -9,7 +9,7 @@ const Text = styled.div<TextProps>`
   color: var(--color-text);
   font-size: ${(props) => {
     if (props.heading) return '48px';
-    if (props.secondary) return '13px';
+    if (props.secondary) return '15px';
     return '16px';
   }};
   opacity: ${(props) => (props.secondary ? '0.56' : '0.87')};
--- src/welcome-screen/components/RecentList.tsx
+++ src/welcome-screen/components/RecentList.tsx
@@ -3,6 +3,14 @@ import styled from 'styled-components';
 import { Text } from '../../elements';
 import { RecentLocation } from '../model';
 
+const EmptyListContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+`;
+
 const OrderedList = styled.ol`
   list-style: none;
   padding: 0;
@@ -41,18 +49,28 @@ interface RecentListProps {
   onSelect: (selected: RecentLocation) => void;
 }
 
-const RecentList: React.FunctionComponent<RecentListProps> = ({ list, onSelect }) => (
-  <OrderedList>
-    {list.map((recentLocation) => (
-      <ListElement key={recentLocation.path}>
-        <Clickable onClick={() => onSelect(recentLocation)}>
-          <Text>{recentLocation.path}</Text>
-          <Text secondary>Number of items: {recentLocation.elementsCount}</Text>
-        </Clickable>
-      </ListElement>
-    ))}
-  </OrderedList>
-);
+const RecentList: React.FunctionComponent<RecentListProps> = ({ list, onSelect }) => {
+  if (list.length === 0) {
+    return (
+      <EmptyListContainer>
+        <Text secondary>There are no recent photo catalogues</Text>
+      </EmptyListContainer>
+    );
+  }
+
+  return (
+    <OrderedList>
+      {list.map((recentLocation) => (
+        <ListElement key={recentLocation.path}>
+          <Clickable onClick={() => onSelect(recentLocation)}>
+            <Text>{recentLocation.path}</Text>
+            <Text secondary>Number of items: {recentLocation.elementsCount}</Text>
+          </Clickable>
+        </ListElement>
+      ))}
+    </OrderedList>
+  );
+};
 
 export default RecentList;
 export { RecentListProps };
