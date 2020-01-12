e55733f86b17d504c1ff1c25d7f53f96e7002971
Deseteral <deseteral@gmail.com>
2020-01-12 13:34:22 +0100

    Button styling

--- src/elements/Button.tsx
+++ src/elements/Button.tsx
@@ -1,25 +1,26 @@
 import styled from 'styled-components';
 
 interface ButtonProps {
+  primary?: boolean;
   onClick: () => void;
-}
 
+}
 const Button = styled.button.attrs((props: ButtonProps) => ({
   type: 'button',
   onClick: props.onClick,
-}))`
+}))<ButtonProps>`
   font-size: 16px;
-  text-transform: uppercase;
+  ${(props) => props.primary && 'text-transform: uppercase;'}
   text-align: center;
   vertical-align: middle;
   white-space: nowrap;
 
   padding: 10px;
 
-  color: var(--color-text-primary);
-  background-color: var(--color-primary);
+  color: ${(props) => (props.primary ? 'var(--color-text-primary)' : 'var(--color-primary)')};
+  background-color: ${(props) => (props.primary ? 'var(--color-primary)' : 'var(--color-background)')};
 
-  border: none;
+  border: 2px solid var(--color-primary);
   border-radius: 12px;
 
   box-shadow: none;
@@ -28,6 +29,8 @@ const Button = styled.button.attrs((props: ButtonProps) => ({
   transition: all .3s ease-in-out;
 
   &:hover, &:focus {
+    color: var(--color-text-primary);
+    border-color: var(--color-primary-dark);
     background-color: var(--color-primary-dark);
     box-shadow: 0 5px 15px rgba(0,0,0,.05), 0 4px 10px var(--color-primary-dark);
   }
