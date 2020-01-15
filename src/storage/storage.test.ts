ceeff5127382f7cfb12e07cb0806c7c403b0bdb6
Deseteral <deseteral@gmail.com>
2020-01-15 08:23:19 +0100

    Debugging

--- src/storage/storage.test.ts
+++ src/storage/storage.test.ts
@@ -4,32 +4,32 @@ import Storage from './storage';
 
 describe('Storage', () => {
   const storagePath = path.resolve(os.tmpdir());
-  const key = 'TEST_KEY';
+  // const key = 'TEST_KEY';
   const storage = new Storage(storagePath);
 
-  beforeEach(async () => {
-    await storage.clear(key);
-  });
+  // beforeEach(async () => {
+  //   await storage.clear(key);
+  // });
 
-  it('should read data', async () => {
-    // given
-    const data = { foo: 'bar' };
-    await storage.set(key, data);
+  // it('should read data', async () => {
+  //   // given
+  //   const data = { foo: 'bar' };
+  //   await storage.set(key, data);
 
-    // when
-    const out = await storage.get(key, {});
+  //   // when
+  //   const out = await storage.get(key, {});
 
-    // then
-    expect(out).toEqual(data);
-  });
+  //   // then
+  //   expect(out).toEqual(data);
+  // });
 
-  it('should return default data if storage is empty', async () => {
-    // when
-    const out = await storage.get(key, { data: 'default' });
+  // it('should return default data if storage is empty', async () => {
+  //   // when
+  //   const out = await storage.get(key, { data: 'default' });
 
-    // then
-    expect(out).toEqual({ data: 'default' });
-  });
+  //   // then
+  //   expect(out).toEqual({ data: 'default' });
+  // });
 
   it('should return data path', () => {
     // when
@@ -39,26 +39,26 @@ describe('Storage', () => {
     expect(dataPath).toBe(storagePath);
   });
 
-  it('should set new data path', () => {
-    const storageWithDifferentPath = new Storage(storagePath);
+  // it('should set new data path', () => {
+  //   const storageWithDifferentPath = new Storage(storagePath);
 
-    // when
-    storageWithDifferentPath.setDataPath('/Some/Path');
+  //   // when
+  //   storageWithDifferentPath.setDataPath('/Some/Path');
 
-    // then
-    expect(storageWithDifferentPath.getDataPath()).toBe('/Some/Path');
-  });
+  //   // then
+  //   expect(storageWithDifferentPath.getDataPath()).toBe('/Some/Path');
+  // });
 
-  it('should clear store', async () => {
-    // given
-    const defaultData = { data: 'default' };
-    await storage.set(key, { some: 'data' });
+  // it('should clear store', async () => {
+  //   // given
+  //   const defaultData = { data: 'default' };
+  //   await storage.set(key, { some: 'data' });
 
-    // when
-    await storage.clear(key);
+  //   // when
+  //   await storage.clear(key);
 
-    // then
-    const data = await storage.get(key, defaultData);
-    expect(data).toEqual(defaultData);
-  });
+  //   // then
+  //   const data = await storage.get(key, defaultData);
+  //   expect(data).toEqual(defaultData);
+  // });
 });
