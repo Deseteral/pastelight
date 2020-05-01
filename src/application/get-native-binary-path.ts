c62c3c088a1a9363e36d41cafd565f618a95bd55
Deseteral <deseteral@gmail.com>
2020-05-01 14:52:29 +0200

    getNativeBinaryPath now works in main and renderer

--- src/application/get-native-binary-path.ts
+++ src/application/get-native-binary-path.ts
@@ -1,13 +1,19 @@
 import path from 'path';
-import { remote } from 'electron';
+import { app, remote } from 'electron';
 // @ts-ignore
 import { rootPath } from 'electron-root-path'; // TODO: Do something with missing declarations
 import isDevMode from './is-dev-mode';
 
+function isRenderer() {
+  return process?.type === 'renderer';
+}
+
 function getNativeBinaryPath(pathInsideNative: string[]) : string {
   const binaryPath = path.join(...pathInsideNative);
 
-  const { getAppPath } = remote.app;
+  const getAppPath = isRenderer()
+    ? remote.app.getAppPath
+    : app.getAppPath;
 
   const binariesPath = !isDevMode()
     ? path.join(path.dirname(getAppPath()), '..', 'Resources', 'binary_deps', binaryPath)
