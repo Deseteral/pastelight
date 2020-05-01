440c29199782792fdef09a4f5f7ef5935a825c07
Deseteral <deseteral@gmail.com>
2020-05-01 17:51:12 +0200

    Don't add duplicate items to library

--- src/library/library.ts
+++ src/library/library.ts
@@ -24,13 +24,24 @@ class Library {
   load() : Promise<void> {
     return new Promise((resolve, reject) => {
       this.db.loadDatabase((err) => {
-        if (guardError(err, reject)) return; // TODO: Handle error
+        if (guardError(err, reject)) return; // TODO: Handle errors
         resolve();
       });
     });
   }
 
-  addNewItem(item: MediaItem) : Promise<void> {
+  async addNewItem(item: MediaItem) : Promise<void> {
+    const savedItem = await this.findOne({ path: item.path });
+    if (!savedItem) {
+      this.insert(item);
+    }
+  }
+
+  getAllItems() : Promise<MediaItem[]> {
+    return this.find({});
+  }
+
+  private insert(item: MediaItem) : Promise<void> {
     return new Promise((resolve, reject) => {
       this.db.insert(item, (err) => {
         if (guardError(err, reject)) return;
@@ -39,11 +50,20 @@ class Library {
     });
   }
 
-  getAllItems() : Promise<MediaItem[]> {
+  private find(query: any) : Promise<MediaItem[]> {
     return new Promise((resolve, reject) => {
-      this.db.find({}, (err: Error, items: MediaItem[]) => {
+      this.db.find(query, (err: Error, items: MediaItem[]) => {
         if (guardError(err, reject)) return;
         resolve(items);
+      });
+    });
+  }
+
+  private findOne(query: any) : Promise<MediaItem> {
+    return new Promise((resolve, reject) => {
+      this.db.findOne(query, (err: Error, item: MediaItem) => {
+        if (guardError(err, reject)) return;
+        resolve(item);
       });
     });
   }
