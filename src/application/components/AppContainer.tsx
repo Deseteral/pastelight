71be597815dbc8af2ae838ab9cf348c36b88cb85
Deseteral <deseteral@gmail.com>
2020-05-05 01:45:41 +0200

    Move AppContainer definition to React syntax

--- src/application/components/AppContainer.tsx
+++ src/application/components/AppContainer.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { ViewRouter, NavigationBar, NavigationProvider } from '../../navigation';
 import AppContextProvider from './AppContextProvider';
 
-function AppContainer() {
-  return (
-    <AppContextProvider>
-      <NavigationProvider>
-        <NavigationBar />
-        <ViewRouter />
-      </NavigationProvider>
-    </AppContextProvider>
-  );
-}
+interface AppContainerProps {}
+const AppContainer: React.FunctionComponent<AppContainerProps> = () => (
+  <AppContextProvider>
+    <NavigationProvider>
+      <NavigationBar />
+      <ViewRouter />
+    </NavigationProvider>
+  </AppContextProvider>
+);
 
 export default AppContainer;
+export { AppContainerProps };
