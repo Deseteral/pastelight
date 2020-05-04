9461ed4db4cff6ad004cfc6497f144b44ff736a2
Deseteral <deseteral@gmail.com>
2020-05-05 01:44:44 +0200

    Simplify AppContext creation

--- src/application/app-context.ts
+++ src/application/app-context.ts
@@ -7,15 +7,14 @@ interface AppContext {
 }
 
 async function createAppContext(libraryPath: string) : Promise<AppContext> {
-  // Load library database
-  const library = new Library(libraryPath);
-  await library.load();
-
   // Create app context
   const appContext = {
     pastelogue: new PastelogueClient(),
-    library,
+    library: new Library(libraryPath),
   };
+
+  // Load library database
+  await appContext.library.load();
 
   // Kick off initial processing
   appContext.pastelogue.on('PROCESSING_PROGRESS', async (progressInfo) => {
